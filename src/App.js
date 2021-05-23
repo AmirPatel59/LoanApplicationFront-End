89bc5a2ae6a3cfbd6c3a331100ee1df44196a08f
Patel <amir-rahim.patel@capgemini.com>
2021-05-23 17:02:57 +0530

    loanIssuerDone

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import LoginLI from './Pages/LoginLI';
 import LoginCs from './Pages/LoginCs';
 import { BrowserRouter as Router,Route, Switch } from 'react-router-dom';
 import Register from './Component/Register';
+import ViewAllLoans from '../src/Component/ViewAllLoans'
+import ViewLoan from './Component/ViewLoan';
 
 
 function App() {
@@ -13,6 +15,9 @@ function App() {
      <Switch>
        <Route exact path="/" component={LoginCs}></Route>
        <Route  path="/register" component={Register}></Route>
+       <Route  path="/LoginIssuer" component={LoginLI}></Route>
+       <Route  path="/viewAllLoans" component={ViewAllLoans}></Route>
+       <Route  path="/viewLoan" component={ViewLoan}></Route>
      </Switch>
      </Router>
     </div>
